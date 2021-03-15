1fbfeb06825d2e2275c6b513429a26c70d6fba4b
Cemal Göymen <goymencemal@gmail.com>
2021-03-15 22:18:31 +0200

    İlk Deneme

---
+++ EhealthCare.API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+using EhealthCare.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace EhealthCare.API.Data
+{
+    public class DataContext : DbContext   
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) {}
+
+        public DbSet<Value> Values {get; set;}
+        
+
+    }
+}
